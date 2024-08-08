5cb208f9d514d896769bbeb1f4f3c5ffae7da4cb
Maja Moskala <m.moskala@evionica.com>
2024-08-08 13:19:26 +0200

    feat: loading page

---
+++ app/loading.tsx
@@ -0,0 +1,15 @@
+'use client'
+import ClipLoader from "react-spinners/ClipLoader"
+
+const styleOverride = {
+    display: 'block',
+    margin: '100px auto'
+}
+
+const Loading: React.FC = () => {
+    return (
+        <ClipLoader color="#3b82f6" cssOverride={styleOverride} size={150} aria-label='Loading' />
+    )
+}
+
+export default Loading
