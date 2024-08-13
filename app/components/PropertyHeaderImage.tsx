94fccd8d00e45fb9c69edf74fd6b0f77287c7f4f
Maja Moskala <m.moskala@evionica.com>
2024-08-13 18:35:46 +0200

    feat: property detail page

---
+++ app/components/PropertyHeaderImage.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const PropertyHeaderImage: React.FC<{image: string}> = async ({ image }) => {
+    return (
+    <section>
+      <div className="container-xl m-auto">
+        <div className="grid grid-cols-1">
+          <img
+            src={`/images/properties/${image}`}
+            alt=""
+            className="object-cover h-[400px] w-full"
+            width={0}
+            height={0}
+            sizes='100vw'
+          />
+        </div>
+      </div>
+    </section>
+    )
+
+}
+
+export default PropertyHeaderImage
--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,3 +1,11 @@
+import connectDB from '@/utils/connectDB'
+import Property from '@/models/Property'
+import { PropertyApiGet } from '@/types/property';
+import PropertyHeaderImage from '@/app/components/PropertyHeaderImage';
+import PropertyDetails from '@/app/components/PropertyDetails';
+import Link from 'next/link';
+import { FaArrowLeft } from 'react-icons/fa';
+
 interface PropertyParams {
     id: string;
 }
@@ -6,8 +14,34 @@ interface PropertyProps {
     params: PropertyParams;
 }
 
-const Property: React.FC<PropertyProps> = ({ params }) => {
-    return <div className="text-2xl">Property {params.id}</div>;
-  };
+const PropertyDetail: React.FC<PropertyProps> = async({ params }) => {
+    await connectDB()
+    const property: PropertyApiGet | null = await Property.findById(params.id).lean()
+    const { images } = property || {}
+
+    return (
+        <>
+        <PropertyHeaderImage image={images?.[0] || ''}></PropertyHeaderImage>
+        <section>
+            <div className="container m-auto py-6 px-6">
+                <Link
+                    href="/properties"
+                    className="text-blue-500 hover:text-blue-600 flex items-center"
+                >
+                    <FaArrowLeft className='mr-2' /> 
+                    Back to Properties
+                </Link>
+            </div>
+        </section>
+        <section className="bg-blue-50">
+            <div className="container m-auto py-10 px-6">
+                <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+                    <PropertyDetails property={property!}></PropertyDetails>
+                </div>
+            </div>
+        </section>
+        </>
+    )
+  }
   
-export default Property
+export default PropertyDetail
