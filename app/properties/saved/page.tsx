85487a9c500ff5744c63cff133eabe4416ccc11f
Maja Moskala <m.moskala@evionica.com>
2024-08-20 19:27:58 +0200

    feat: saved properties page

---
+++ app/properties/saved/page.tsx
@@ -0,0 +1,29 @@
+import PropertyCard from "@/app/components/PropertyCard"
+import User from "@/models/User"
+import { PropertyApiGet } from "@/types/property"
+import connectDB from "@/utils/connectDB"
+import { getSessionUser } from "@/utils/getSessionUser"
+
+const PropertiesSaved = async () => {
+    await connectDB()
+
+    const {id: userId} = await getSessionUser() || {}
+    if (!userId) throw new Error('User.id is missing')
+
+    const { favorites } = await User.findById(userId).populate('favorites')
+
+    return (
+        <section className="container lg:container m-auto px-4 py-6">
+            <h1 className="text-2xl mb-4">Saved properties</h1>
+            {favorites.length === 0 ? (<p>No saved properties</p>) : (
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                    {favorites.map((property: PropertyApiGet) => {
+                        return <PropertyCard key={property._id} property={property}/>
+                    })}
+                </div>
+            )}
+        </section>
+    )
+}
+
+export default PropertiesSaved
