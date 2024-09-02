1948da8d21ee3a9b9b4c1cd6e949ee0a43a52fc2
Maja Moskala <m.moskala@evionica.com>
2024-09-02 13:30:54 +0200

    fix: middleware

--- middleware.js
+++ middleware.js
@@ -1,6 +1,5 @@
 export { default } from 'next-auth/middleware'
-import { propertiesAddPath, profilePath, propertiesSavedPath, messagesPath } from './utils/paths'
 
 export const config = {
-    matcher: [propertiesAddPath, profilePath, propertiesSavedPath, messagesPath]
+    matcher: ['/properties/add', '/profile', '/properties/save', '/messages']
 }
